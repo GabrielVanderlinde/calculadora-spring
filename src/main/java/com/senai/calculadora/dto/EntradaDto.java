4c81f9113742300862f9594dbcfe1e6be40160c6
GABRIEL HENRIQUE VANDERLINDE <gabriel_vanderlinde@estudante.sesisenai.org.br>
2026-03-02 20:28:55 -0300

    feat: cria estrutura e dto

--- src/main/java/com/senai/calculadora/dto/EntradaDto.java
+++ src/main/java/com/senai/calculadora/dto/EntradaDto.java
@@ -1,4 +1,27 @@
 package com.senai.calculadora.dto;
 
 public class EntradaDto {
+    private Double numero1;
+    private Double numero2;
+
+    //Contructor
+    public EntradaDto() {
+    }
+
+    //Get And Setter
+    public Double getNumero1() {
+        return numero1;
+    }
+
+    public void setNumero1(Double numero1) {
+        this.numero1 = numero1;
+    }
+
+    public Double getNumero2() {
+        return numero2;
+    }
+
+    public void setNumero2(Double numero2) {
+        this.numero2 = numero2;
+    }
 }
--- src/main/java/com/senai/calculadora/dto/ResultadoDto.java
+++ src/main/java/com/senai/calculadora/dto/ResultadoDto.java
@@ -1,4 +1,37 @@
 package com.senai.calculadora.dto;
 
 public class ResultadoDto {
+
+    private Double numero;
+    private Double getNumero;
+    private Double resultado;
+
+    //Constructor
+    public ResultadoDto() {
+    }
+
+    //Get and Set
+    public Double getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Double numero) {
+        this.numero = numero;
+    }
+
+    public Double getGetNumero() {
+        return getNumero;
+    }
+
+    public void setGetNumero(Double getNumero) {
+        this.getNumero = getNumero;
+    }
+
+    public Double getResultado() {
+        return resultado;
+    }
+
+    public void setResultado(Double resultado) {
+        this.resultado = resultado;
+    }
 }
