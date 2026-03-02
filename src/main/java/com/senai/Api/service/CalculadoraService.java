16ba9a4be3c1bb340ed525bb2c10bd84a62b17d2
GABRIEL HENRIQUE VANDERLINDE <gabriel_vanderlinde@estudante.sesisenai.org.br>
2026-03-02 19:05:28 -0300

    feat: adiciona estrutura de operacoes matematicas service

--- src/main/java/com/senai/Api/service/CalculadoraService.java
+++ src/main/java/com/senai/Api/service/CalculadoraService.java
@@ -1,4 +1,28 @@
 package com.senai.Api.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class CalculadoraService {
+
+    public double adicao(double n1, double n2) {
+        return n1 + n2;
+    }
+
+    public double subtracao(double n1, double n2) {
+        return n1 - n2;
+    }
+
+    public double multiplicacao(double n1, double n2) {
+        return n1 * n2;
+    }
+
+    public double divisao(double n1, double n2) {
+        if (n1 == 0 || n2 == 0) {
+            throw new IllegalArgumentException("Divisão por Zero Impossível");
+        }
+        return n1 / n2;
+    }
+
+
 }
