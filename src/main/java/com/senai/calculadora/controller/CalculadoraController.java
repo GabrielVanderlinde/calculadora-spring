cc0687525b936d64e9d554c6599b05831a220313
GABRIEL HENRIQUE VANDERLINDE <gabriel_vanderlinde@estudante.sesisenai.org.br>
2026-03-02 20:13:59 -0300

    feat: cria estrutura e base incial controller

--- src/main/java/com/senai/calculadora/controller/CalculadoraController.java
+++ src/main/java/com/senai/calculadora/controller/CalculadoraController.java
@@ -1,9 +1,32 @@
 package com.senai.calculadora.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-
+@RequestMapping("/calculadora")
 public class CalculadoraController {
 
+    @PostMapping("/adicionar")
+    public ResponseEntity<String> adicionar() {
+        return ResponseEntity.ok().body("");
+    }
+
+    @PostMapping("/subtrair")
+    public ResponseEntity<String> subtrair() {
+        return ResponseEntity.ok().body("");
+    }
+
+
+    @PostMapping("/multiplicar")
+    public ResponseEntity<String> multiplicar() {
+        return ResponseEntity.ok().body("");
+    }
+
+    @PostMapping("/dividir")
+    public ResponseEntity<String> dividir() {
+        return ResponseEntity.ok().body("");
+    }
 }
